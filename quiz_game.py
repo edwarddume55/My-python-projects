a6903997d750ddec5c9fc03403294b5c0fa3cf01
Edward Dume <edwarddume55@gmail.com>
2025-01-27 13:19:24 +0300

    guess game project

--- quiz_game.py
+++ quiz_game.py
@@ -7,33 +7,38 @@ if playing.upper() != 'YES':
     quit()
 print("Okay let's start:")
 score = 0
+correct = 0
 
-answer = input("What does GIF statnds for? ")
+answer = input("What does GIF stands for? ")
 if answer.lower() == "graphics interchange format":
     print('Correct')
-    score += 1
+    score += 5
+    correct += 1
 else:
     print('Incorrect')
 
-answer = input("What does CPU statnds for? ")
+answer = input("What does CPU stands for? ")
 if answer.lower() == "central processing unit":
     print('Correct')
-    score += 1
+    score += 5
+    correct +=1
 else:
     print('Incorrect')
 
-answer = input("What does RAM statnds for? ")
+answer = input("What does RAM stands for? ")
 if answer.lower() == "random access memory":
     print('Correct')
-    score += 1
+    score += 5
+    correct +=1
 else:
     print('Incorrect')
 
-answer = input("What does ROM statnds for? ")
+answer = input("What does ROM stands for? ")
 if answer.lower() == "read only memory":
     print('Correct')
-    score += 1
+    score += 5
+    correct +=1
 else:
     print('Incorrect')
 
-print( 'you got ' + str(score) + ' correct' + 'which is ' + str((score/4)*100) + ('%.'))
+print( 'you got ' + str(correct) + ' correct' + 'which is ' + str((score/20)*100) + ('%.'))
